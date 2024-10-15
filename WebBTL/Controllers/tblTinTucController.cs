7566887fe75ee0421915348786434c43ca9d9264
hoang12 <hoangluucoder1702@gmail.comgit config --global user.email hoangluucoder1702@gmail.com>
2024-10-15 08:54:04 +0700

    detail tin tuc

--- WebBTL/Controllers/tblTinTucController.cs
+++ WebBTL/Controllers/tblTinTucController.cs
@@ -46,7 +46,19 @@ namespace WebBTL.Controllers
                 return RedirectToAction("Index");
             }
 
-            
+            // Tách các tags của bài viết hiện tại thành mảng
+            var tags = tintuc.Tags.Split(',');
+
+            // Lấy ra các bài viết liên quan có cùng tag, ngoại trừ bài viết hiện tại
+            var lsBaiVietLienQuan = _context.tblTinTucs
+                .AsNoTracking()
+                .Where(x => x.Published == true
+                            && x.PostID != id
+                            && tags.Any(tag => x.Tags.Contains(tag))) // Kiểm tra bài viết có chứa bất kỳ tag nào trong bài viết hiện tại
+                .OrderByDescending(x => x.CreateDate) // Sắp xếp theo ngày tạo
+                .Take(3) // Lấy 3 bài viết
+                .ToList();
+            ViewBag.lsBaiVietLienQuan = lsBaiVietLienQuan;
             return View(tintuc);
         }
     }
